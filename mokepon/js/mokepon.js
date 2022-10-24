6ab73ea9d0f397aaf872becaca9ab551c0a447f3
Enrique Morales <enrique.moralesv2@gmail.com>
2022-10-23 20:06:12 -0500

    Now we automated the function of select enemy

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -7,17 +7,33 @@ function selecionarMascotaJugador(){
    let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
+    
     if (inputHipodoge.checked){
-        alert('Seleccionaste a Hipodoge')
+        spanMascotaJugador.innerHTML = 'Hipodoge'
     } else if (inputCapipepo.checked){
-        alert('Seleccionaste a Capipepo')
+        spanMascotaJugador.innerHTML = 'Capipepo'
     } else if(inputRatigueya.checked){
-        alert('Seleccionaste a Ratigueya')
+        spanMascotaJugador.innerHTML = 'Ratigueya'
     } else {
         alert('No haz seleccionado ninguna mascota')
     }
-        
+    selecionarMascotaEnemigo()
+}
+    function selecionarMascotaEnemigo(){
+        let ataqueAleatorio = aleatorio(1, 3)
+        let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+        if  (ataqueAleatorio == 1){
+            spanMascotaEnemigo.innerHTML = 'Hipodoge'
+        } else if (ataqueAleatorio == 2){
+            spanMascotaEnemigo.innerHTML = 'Capipepo'
+        } else {
+            spanMascotaEnemigo.innerHTML = 'Ratigueya'
+        }
     }
 
-
+    function aleatorio(min, max){
+        return Math.floor(Math.random() * (max - min +1) + min)
+    }
+    
 window.addEventListener('load', iniciarjuego)
