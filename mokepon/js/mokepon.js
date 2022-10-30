4524bc68be87f62a0e96625b935af5eb1b3379e6
Enrique Morales <enrique.moralesv2@gmail.com>
2022-10-30 18:28:38 -0500

    Functions os Attack

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,16 @@
+let ataqueJugador
+let ataqueEnemigo
+
 function iniciarjuego(){
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', selecionarMascotaJugador)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click',ataqueFuego)
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.addEventListener('click',ataqueAgua)
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.addEventListener('click',ataqueTierra)
 }
 
 function selecionarMascotaJugador(){
@@ -21,18 +31,39 @@ function selecionarMascotaJugador(){
     selecionarMascotaEnemigo()
 }
     function selecionarMascotaEnemigo(){
-        let ataqueAleatorio = aleatorio(1, 3)
+        let mascotaAleatoria = aleatorio(1, 3)
         let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
-        if  (ataqueAleatorio == 1){
+        if  (mascotaAleatoria == 1){
             spanMascotaEnemigo.innerHTML = 'Hipodoge'
-        } else if (ataqueAleatorio == 2){
+        } else if (mascotaAleatoria == 2){
             spanMascotaEnemigo.innerHTML = 'Capipepo'
         } else {
             spanMascotaEnemigo.innerHTML = 'Ratigueya'
         }
     }
-
-    function aleatorio(min, max){
+function ataqueFuego (){
+    ataqueJugador = 'FUEGO'
+    ataqueAleatorioEnemigo()
+}
+function ataqueAgua(){
+    ataqueJugador = 'AGUA'
+    ataqueAleatorioEnemigo()
+}
+function ataqueTierra(){
+    ataqueJugador = 'TIERRA'
+    ataqueAleatorioEnemigo()
+}
+function ataqueAleatorioEnemigo (){
+    let ataqueAleatorio = aleatorio(1,3)
+    if(ataqueAleatorio == 1){
+        ataqueEnemigo = 'FUEGO'
+    } else if (ataqueAleatorio == 2){
+        ataqueEnemigo = 'AGUA'
+    } else {
+        ataqueEnemigo = 'TIERRA'
+}
+}
+function aleatorio(min, max){
         return Math.floor(Math.random() * (max - min +1) + min)
     }
     
